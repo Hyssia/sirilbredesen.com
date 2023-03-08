9c846c9a33881e3b53d9e20b2c325178b8df1c76
Anders Bredesen <nexpex@hotmail.com>
2023-03-08 10:57:54 +0100

    threshold update

--- assets/fade.js
+++ assets/fade.js
@@ -1,7 +1,7 @@
 const faders = document.querySelectorAll('.fade-in');
  
 const appearOptions = { 
-    threshold: 1,
+    threshold: 0.9,
     rootMargin: "0px 0px -200px 0px"
 };
 const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
